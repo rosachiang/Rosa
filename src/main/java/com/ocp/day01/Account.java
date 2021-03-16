e30127355ede3fa99fe12da88818e2e7ac3893de
MB-study <MB-study@192.168.1.153>
2021-03-16 21:39:35 +0800

    It's my first class

--- src/main/java/com/ocp/day01/Account.java
+++ src/main/java/com/ocp/day01/Account.java
@@ -3,10 +3,25 @@ package com.ocp.day01;
 
 
 public class Account {
- int balance;// 帳戶餘額    
+ private int balance;// 帳戶餘額  
+       //自行撰寫一個帳戶餘額方式
+        int getBalance(){
+            return balance; // 方法停止並回傳餘額
+        }
+       
+       
  void withdraw(int money ){ //提款方法
+     //提款方法
+     if(money<=0){
+         System.out.println("提款金額有誤!");
+         return; //提款到此結束
+     }
+     if(money>balance){
+         System.out.println("帳戶餘額不足");
+         return;
+     }
      balance-= money;
- }
+    }
  
  void deposit(int money){ //存款方法
      //VERTIFY MONEY COULDN'T BE -
--- src/main/java/com/ocp/day01/Bank.java
+++ src/main/java/com/ocp/day01/Bank.java
@@ -2,27 +2,30 @@
 package com.ocp.day01;
 public class Bank {
       public static void main(String[] args) {
-        Account a01=new Account();
+        Account a01=new Account();//建立物件
         Account a02=new Account();
         Account a03=new Account();
-        a01.balance=1000;
-        a02.balance=2000;
-        a03.balance=3000;
-          System.out.printf("a01帳戶餘額: $%,d\n",a01.balance);
-          System.out.printf("a02帳戶餘額: $%,d\n",a02.balance);
-          System.out.printf("a03帳戶餘額: $%,d\n",a03.balance);
+        //當成各帳戶第一次的存款
+        a01.deposit(1000);
+        a02.deposit(2000);
+        a03.deposit(3000);
+        
+          System.out.printf("a01帳戶餘額: $%,d\n",a01.getBalance());
+          System.out.printf("a02帳戶餘額: $%,d\n",a02.getBalance());
+          System.out.printf("a03帳戶餘額: $%,d\n",a03.getBalance());
           
           //deposit
           System.out.println("存款後...");
           a01.deposit(500);
-          System.out.printf("a01 帳戶餘額: $%d\n", a01.balance);
+          System.out.printf("a01 帳戶餘額: $%d\n", a01.getBalance());
           a02.withdraw(800);
-          System.out.printf("a02 帳戶餘額: $%d,\n", a02.balance);
+          System.out.printf("a02 帳戶餘額: $%d,\n", a02.getBalance());
           //存款
           System.out.println("a03 存款-1200 元後....");
           a03.deposit(-1200);
-          System.out.printf("a03 帳戶餘額:$%,d\n", a03.balance);
-          
+          System.out.printf("a03 帳戶餘額:$%,d\n", a03.getBalance());
+          a03.withdraw(5000);
+          System.out.printf("a03 帳戶餘額:$%,d\n", a03.getBalance());
           
           
     }     
